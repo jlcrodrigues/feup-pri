92ff5c8ec7db9ff76a6d547436a868b7046084cb
luis <joseluiscunharodrigues@gmail.com>
2023-10-30 12:21:12 +0000

    Created script to upload json to Solr

---
+++ src/startup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Source the .env file to load the environment variables
+if [ -f ".env" ]; then
+    source .env
+else
+    echo ".env file not found. Make sure it's in the same directory as this script."
+    exit 1
+fi
+
+docker compose up -d
+
+# Use environment variables to set ports and core name
+SOLR_PORT="${SOLR_PORT:-8983}"  # Default to 8983 if not set in .env
+
+# Split the list into an array
+IFS=',' read -ra cores <<< "$CORES"
+IFS=',' read -ra schemas <<< "$SCHEMA_FILES"
+IFS=',' read -ra files <<< "$DATA_FILES"
+
+for ((i = 0; i < ${#cores[@]}; i++)); do
+    core="${cores[i]}"
+    schema="${schemas[i]}"
+    file="${schemas[i]}"
+
+    # Delete core
+    curl -X DELETE "http://localhost:$SOLR_PORT/solr/admin/cores?action=UNLOAD&core=$core&deleteDataDir=true&deleteIndex=true&deleteInstanceDir=true"
+
+    # Create core
+    docker exec -it my_solr solr create_core -c $core 
+    
+    # Post schema
+    curl -X POST -H 'Content-type:application/json' \
+        --data-binary "@./json/$schema" \
+        "http://localhost:$SOLR_PORT/solr/$core/schema"
+
+    # Post data
+    curl -X POST -H 'Content-type:application/json' \
+        --data-binary "@./json/meic_courses.json" \
+        http://localhost:8983/solr/$core/update?commit=true
+done
+
