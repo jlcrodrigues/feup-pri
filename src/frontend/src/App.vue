99ba6b82482b2c1c9bc1871028c9b34de05093bc
luis <joseluiscunharodrigues@gmail.com>
2023-11-25 11:03:32 +0000

    Added vue.js base project

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+import HomeView from './views/HomeView.vue'
+
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
